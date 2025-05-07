28dbcf33f75905a227af9caf342b0707c501c586
TANHEHANZ <hantach10@gmail.com>
2025-05-07 12:50:51 -0400

    form data

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const multer_1 = __importDefault(require("multer"));
 const app = (0, express_1.default)();
 const manage_1 = __importDefault(require("./manage"));
+const upload = (0, multer_1.default)();
 app.use(express_1.default.json());
+// app.use(express.urlencoded({ extended: true }));
+app.use(upload.none());
 app
     .get("/", (req, res) => {
     res.send("¡Servidor funcionando correctamente!");
