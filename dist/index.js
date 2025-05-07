fc68bf5ca3f83082a585eea3c31ad7e526255e49
Alejandro_Camacho <alejandro.camacho.dev.01@gmail.com>
2025-05-07 06:26:52 -0400

    configuracion

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const manage_1 = __importDefault(require("./manage"));
+app.use(express_1.default.json());
+app
+    .get("/", (req, res) => {
+    res.send("¡Servidor funcionando correctamente!");
+})
+    .use("/api", manage_1.default);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
